714f6a604c6602aabc7f51073fc492e96d6b7238
Kevin Steele <kevindsteeleii@gmail.com>
2018-07-30 06:08:51 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+def dictionary (word)
+  dict = {
+    "hello" => 'hi',
+    'to' => 2,
+    'two' => 2,
+    'too' => 2,
+    'four' => 4,
+    'for' => 4,
+    'be' => 'b',
+    'you' => 'u',
+    'at' => '@',
+    'and' => '&'
+  }
+  dict[word] || word
+end
+def word_substituter (tweet)
+  tweet.to_s.split(' ').map do |word|
+    dictionary(word)
+  end.join(' ')
+end
+
+def bulk_tweet_shortener (tweet)
+  # puts tweet.chars[0...140].join
+  word_substituter(tweet)
+end
+
